b0ca4fcb5b839438210da83561daee668da36eef
Zhakhonsha Ikramov <jahonshoh.ikramov@gmail.com>
2026-01-05 18:07:29 +0500

    Update project files

---
+++ src/script.js
@@ -0,0 +1,17 @@
+class Todo {
+    selectors = {
+        root: '[date-js-todo]',
+        newTaskForm: '[date-js-todo-new-task-form]',
+        newTaskInput: '[date-js-todo-new-task-input]',
+        searchTaskForm: '[date-js-todo-search-task-form]',
+        searchTaskInput: '[date-js-todo-search-task-input]',
+        totalTasks: '[date-js-todo-total-tasks]',
+        deleteAllButton: '[date-js-todo-delete-all-button]',
+        list: '[date-js-todo-list]',
+        item: '[date-js-todo-item]',
+        itemCheckbox: '[date-js-todo-item-checkbox]',
+        itemLabel: '[date-js-todo-item-label]',
+        itemDeleteButton: '[date-js-todo-item-delete-button]',
+        emptyMessage: '[date-js-todo-empty-message]',
+    }
+}
